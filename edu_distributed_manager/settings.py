d9c3bd25b4be44be527b9ba1dd86531fd03d75a8
Kevin Alexis Rodríguez Condori <101363038+KEVIN-117@users.noreply.github.com>
2025-10-23 11:09:53 -0400

    Revert "feat: Implement user profile creation with Celery integration and add…"
    This reverts commit 20fc2e15c11d69378ecfc46cffe19e47352486a8.

--- edu_distributed_manager/settings.py
+++ edu_distributed_manager/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_celery_results',
-    'academic'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +74,6 @@ WSGI_APPLICATION = 'edu_distributed_manager.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-REDIS_URL = f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/{os.getenv('REDIS_DB')}"
-
 
 DATABASES = {
     'default': {
@@ -87,14 +83,6 @@ DATABASES = {
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
         'PORT': os.getenv('POSTGRES_PORT'),
-    },
-}
-
-CACHES = {
-    "default": {
-        'ENGINE': 'django_redis.cache.RedisCache',
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        'LOCATION': REDIS_URL,
     }
 }
 
@@ -133,18 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Celery Configuration Options
-CELERY_TIMEZONE = "Australia/Tasmania"
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_CACHE_BACKEND = 'default'
-CELERY_TASK_TRACK_STARTED = True
-CELERY_TASK_TIME_LIMIT = 30 * 60
